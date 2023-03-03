89ad3c21c78ef4fac49fce4a8f34926c852723bd
Caleb Krauter <krautercaleb@gmail.com>
2023-03-03 12:35:53 -0800

    Implement basic GUI with 3x3 set of cells set to 0 and are editable.

---
+++ src/view/CellsPanel.java
@@ -0,0 +1,60 @@
+package view;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+// TODO - review the overall code and update this code.
+public class CellsPanel extends JPanel {
+
+    private int amount = 9;
+    private int columnAmount = 3;
+    private int rowAmount = 3;
+    JTextField[] arrayOfCells;
+    LayoutManager gridLayout;
+
+    public CellsPanel() {
+        arrayOfCells = new JTextField[amount];
+        gridLayout = new GridLayout(rowAmount, columnAmount, 1, 1);
+        int i = 0;
+        setLayout(gridLayout);
+        produceDataBox(amount, i);
+        listeners();
+    }
+
+    private void listeners() {
+        // Look at all the cells and add an action to each.
+        for (int j = 0; j < amount; j++) {
+            final int val = j;
+            arrayOfCells[j].addActionListener(new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    setInfo(val);
+                }
+            });
+        }
+    }
+
+    private void setInfo(int j) {
+        // TODO - replace contents with the appropriate setter to update the formula.
+        System.out.println(arrayOfCells[j].getText() + " input from current cell is set.");
+    }
+    public String getInitialValue() {
+        // TODO - replace return statement with the appropriate getter.
+        return "0";
+    }
+
+    private void produceDataBox(int amount, int i) {
+        if (amount == 0) {
+            return;
+        }
+        JTextField currentField = new JTextField(getInitialValue());
+        add(currentField);
+
+        // Save the current cellField to an array to be accessed later.
+        arrayOfCells[i] = currentField;
+        amount--;
+        i++;
+        produceDataBox(amount, i);
+    }
+
+}
--- src/view/DisplayGui.java
+++ src/view/DisplayGui.java
@@ -2,52 +2,40 @@ package view;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.IOException;
 
-import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
-
-
-public class DisplayGui {
+public class DisplayGui extends JFrame{
 
     private final Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
-    private SpreadSheetPanel spreadSheetPanel;
-    private int rows = 10;
-    private int columns = 10;
-    private GridLayout gridLayout;
     private JFrame window;
+    private CellsPanel cellsPanel;
+    public DisplayGui () {
 
-    public DisplayGui() throws IOException{
-        spreadSheetPanel = new SpreadSheetPanel(gridLayout);
-        displayWindow();
+        cellsPanel = new CellsPanel();
+        loadGUi();
     }
 
-    // TODO - pass in any necessary panel(s)
-    public void displayWindow() {
+    public void loadGUi() {
+
+// TODO - pass in any necessary panel(s)
         window = new JFrame();
 
         window.setDefaultCloseOperation(EXIT_ON_CLOSE);
         window.setLocation(SCREEN_SIZE.width / 2 - window.getWidth() / 2,
                 SCREEN_SIZE.height / 2 - window.getHeight() / 2);
         window.setSize(850, 850);
-        window.setMinimumSize(new Dimension(750, 750));
+        window.setMinimumSize(new Dimension(400, 400));
 
-        window.setLayout(new GridLayout(rows, columns, 1, 1));
-        produceDataBox(100);
+//    window.setLayout(new GridLayout(rows, columns, 1, 1));
 
+//        Cell cell = new Cell();
+//        produceDataBox(100);
+
+//    cellField1 = new JTextField();
+        window.add(cellsPanel);
 
         window.pack();
         window.setVisible(true);
 
     }
-
-    private void produceDataBox(int amount) {
-            if (amount == 0) {
-                return;
-            }
-                window.add(new JTextField());
-                amount--;
-                produceDataBox(amount);
-    }
-
 
 }
