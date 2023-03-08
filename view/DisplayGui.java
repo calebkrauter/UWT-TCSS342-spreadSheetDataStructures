a661dbd9ddf25ac1c732fbb2d4d06e2c10f8aecf
Caleb Krauter <krautercaleb@gmail.com>
2023-03-08 14:34:19 -0800

    Update javadoc in view

--- view/DisplayGui.java
+++ view/DisplayGui.java
@@ -1,23 +1,51 @@
+/**
+ * @author Caleb Krauter
+ */
 package view;
 
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Displays the window that has a scrollable panel for panels to be added too.
+ */
 public class DisplayGui extends JFrame {
-    
+
+    /**
+     * Size of the screen in use.
+     */
     private final Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
+
+    /**
+     * A window used to display GUI elements.
+     */
     private JFrame window;
+
+    /**
+     * A panel that contains other panels to make up the spreadsheet GUI.
+     */
     private SpreadSheetPanel spreadSheetPanel;
-    
+
+    /**
+     * A constructor for DisplayGui which initializes
+     * the panels and loads the GUI.
+     */
     public DisplayGui () {
         initializePanels();
         loadGUI();
     }
-    
+
+    /**
+     * A function used to modularize the initialization
+     * of any panels in this class.
+     */
     private void initializePanels() {
         spreadSheetPanel = new SpreadSheetPanel();
     }
-    
+
+    /**
+     * A function that loads the window to hold the GUI elements.
+     */
     public void loadGUI() {
         window = new JFrame();
         window.setDefaultCloseOperation(EXIT_ON_CLOSE);
--- view/LabelColumns.java
+++ view/LabelColumns.java
@@ -1,26 +1,53 @@
+/**
+ * @author Caleb Krauter
+ * @author Andy Comfort
+ */
+
 package view;
 
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Adds labels to columns of the spreadsheet.
+ */
 public class LabelColumns extends JPanel {
+
+    /**
+     * An array that contains labels for the columns.
+     */
     final JLabel[] arrayOfLabels;
+
+    /**
+     * A gridLayout used for organizing the labels in reference to the cells.
+     */
     final LayoutManager gridLayout;
-    
-    private final Dimension cellSize = new Dimension(75, 25);
-    
+
+    /**
+     * A reference and declaration to the size of a given label.
+     */
+    private final Dimension labelSize = new Dimension(75, 25);
+
+    /**
+     * A function used to set up the layout and produce the labels.
+     * @param columnAmount
+     */
     public LabelColumns(int columnAmount) {
         arrayOfLabels = new JLabel[columnAmount];
         gridLayout = new GridLayout(0, columnAmount + 1, 0, 0);
         setLayout(gridLayout);
         produceDataBox(columnAmount);
     }
-    
+
+    /**
+     * Produces data boxes, in this class data boxes are labels.
+     * @param amount
+     */
     private void produceDataBox(int amount) {
         JLabel currentLabel = new JLabel("");
         renderVisuals(currentLabel);
         add(currentLabel);
-        
+
         for (int i = 0; i < amount; i++) {
             currentLabel = new JLabel(getColumnLabel(i), SwingConstants.CENTER);
             renderVisuals(currentLabel);
@@ -28,15 +55,24 @@ public class LabelColumns extends JPanel {
             arrayOfLabels[i] = currentLabel;
         }
     }
-    
+
+    /**
+     * Updates the visuals of the current label.
+     * @param currentLabel
+     */
     private void renderVisuals(JLabel currentLabel) {
         currentLabel.setFont(new Font("Verdana", Font.BOLD, 18));
         currentLabel.setBackground(Color.GREEN);
         currentLabel.setOpaque(true);
         currentLabel.setBorder(BorderFactory.createLineBorder(Color.black));
-        currentLabel.setPreferredSize(cellSize);
+        currentLabel.setPreferredSize(labelSize);
     }
-    
+
+    /**
+     * Generates a column label to be displayed from A to at least ZZ.
+     * @param i
+     * @return a String value representing the text label of the column.
+     */
     public String getColumnLabel(int i) {
         int colVal = i + 1;
         
